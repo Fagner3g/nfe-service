20a7f464af26edb82c13dcb39b1eeccae22bc522
Fagner Gomes <fagner.egomes@gmail.com>
2024-07-02 13:40:13 -0300

    login implementation

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,6 @@ import routes from './routes';
 import fastifyCors from '@fastify/cors';
 const app = Fastify({ logger: true });
 
-app.setErrorHandler((error, request, reply) => {
-  reply.code(400).send({ message: error.message });
-});
-
 const start = async () => {
   await app.register(fastifyCors);
   await app.register(routes);
